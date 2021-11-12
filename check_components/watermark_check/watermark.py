bacb085c5b19489138603cbed76b5cecfe25f6a6
Howard <howardyanghaowen@gmail.com>
2021-11-12 13:20:32 +0800

    fitler out watermark instances when background is not white and if watermark is specular reflection

--- check_components/watermark_check/watermark.py
+++ check_components/watermark_check/watermark.py
@@ -4,10 +4,23 @@ from detectron2.data import MetadataCatalog
 from detectron2 import model_zoo
 from detectron2.engine import DefaultPredictor
 from detectron2.config import get_cfg
+from detectron2.structures import Instances
 from detectron2.utils.visualizer import Visualizer
 from detectron2.utils.visualizer import ColorMode
+import numpy as np
+
+def overlap_percentage(watermark, mask):
+    """
+    watermark, mask: numpy.ndarray of type bool
+    returns: [0, 1] percentage of overlap
+    """
+    overlap = np.logical_and(watermark, mask)
+    return overlap.sum() / watermark.sum()
 
 def check_watermark(cfg, input_im, face_foreground_background_res, background_check_result, face_highlight_res):
+    status = 0
+    remarks = 'Watermark detected.'
+
     model_cfg = get_cfg()
     model_cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_X_101_32x8d_FPN_3x.yaml"))
     model_cfg.MODEL.ROI_HEADS.NUM_CLASSES = 2
@@ -24,22 +37,50 @@ def check_watermark(cfg, input_im, face_foreground_background_res, background_ch
         model_cfg.MODEL.DEVICE = "cpu"
 
     predictor = DefaultPredictor(model_cfg)
-    processed_img = predictor(input_im)
+    output = predictor(input_im)
 
-    print(f"Size: {face_foreground_background_res[:,:,0].size}")
-    print(f"Size: {len(face_foreground_background_res[:,:,0].reshape(-1))}")
-    
-    print("Highlight Binary Mask")
-    print(len(face_highlight_res["res_highlight_mask"].reshape(-1)))
+    selected_idx = []
+    for i in range(len(output['instances'].pred_masks)):
+        instance = output['instances'].pred_masks[i].cpu().numpy().reshape(-1)
+
+        # don't add instance if background is not white and watermark instance is mostly in the background
+        # need to invert the face_foreground_background_res mask because the background is black
+        if (background_check_result["status"] == 0 and 
+            overlap_percentage(instance, ~face_foreground_background_res[:,:,0].reshape(-1)) > 0.9):
+                continue
+
+        # don't add instance if watermark is mostly a specular reflection
+        if overlap_percentage(instance, face_highlight_res["res_highlight_mask"].reshape(-1) > 0.9):
+            continue
+
+        # add to selected index
+        selected_idx.append(i)
+
+    o = output['instances']
+    classes = o.pred_classes[selected_idx]
+    scores = o.scores[selected_idx]
+    boxes = o.pred_boxes[selected_idx]
+    masks = o.pred_masks[selected_idx]
+
+    filtered_instances = Instances(image_size=(618, 516))
+
+    filtered_instances.set('pred_classes', classes)
+    filtered_instances.set('scores', scores)
+    filtered_instances.set('pred_boxes', boxes)
+    filtered_instances.set('pred_masks', masks)
 
     v = Visualizer(input_im[:, :, ::-1],
                     metadata=MetadataCatalog.get("watermarks_train"),
                     scale=1,
                     instance_mode=ColorMode.IMAGE_BW  # remove the colors of unsegmented pixels
                     )
-    v = v.draw_instance_predictions(processed_img["instances"].to("cpu"))
+    v = v.draw_instance_predictions(filtered_instances.to("cpu"))
 
-    return {'status': 0, 'remarks': 'Watermark detected.'}, cv2.cvtColor(v.get_image()[:, :, ::-1], cv2.COLOR_BGR2RGB)
+    if len(filtered_instances) == 0:
+        status = 1
+        remarks = 'No watermark detected.'
+
+    return {'status': status, 'remarks': remarks}, cv2.cvtColor(v.get_image()[:, :, ::-1], cv2.COLOR_BGR2RGB)
 
 if __name__ == '__main__':
 
